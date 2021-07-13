fc8ae974d42ffa4473372c2e501bf9de4a1eae69
Rafa Rivas <rafarivas888@hotmail.com>
2021-07-13 03:27:15 +0200

    Signin Page and dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,39 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
+<script>
+export default {
+  name: "App",
+  mounted() {
+    const isDarkMode = this.$store.getters.isDarkMode;
+    document.body.style.background = isDarkMode ? "#212c4f" : "#f0f3f5";
+  },
+};
+</script>
+
+
 <style lang="scss">
+body {
+  background-color: $dark-blue;
+}
+
+h1 {
+  @include heading-1;
+}
+
+p {
+  @include large-text-bold;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: $system-font-family;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  background-color: black;
+  color: $white;
 }
 
 #nav {
@@ -23,7 +41,7 @@
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: $white;
 
     &.router-link-exact-active {
       color: #42b983;
